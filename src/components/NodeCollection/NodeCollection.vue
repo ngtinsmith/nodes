bd8f371c5fef0b207b2040899761b3c62f42476d
ng <ng@192.168.1.13>
2023-08-08 11:10:26 +0800

    refactor: decouple node tree expand/collapse handler
    - move expand/collapse state to store
    - create specific column for node expand/collapse toggles
    - add `addNode` function
    - add `deleteNode` function

--- src/components/NodeCollection/NodeCollection.vue
+++ src/components/NodeCollection/NodeCollection.vue
@@ -1,57 +1,53 @@
 <script setup lang="ts">
-/*
-    TODO
-
-    fix vue script comments
-    tw classes on specific style helper / syntax
-    tw class order
-    tw order prettier
-    fix catpuccin TODO colorscheme
-
-
-          (idx > 0 && idx === node.children.length - 1 && childNode.children.length > 0) ||
-          (idx === 0 && childNode.children.length === 1 && childrenSize === 1)
-*/
-
-import { computed } from 'vue'
-import NodeItem from './NodeItem.vue'
-import type { Node } from './nodes'
-import { rootIds, buildTree } from './nodes'
+import { computed } from 'vue';
+import { useNodes } from '@/stores/nodes';
+import ChevronRight from '/public/assets/icons/chevron-right.svg?component';
+import ChevronDown from '/public/assets/icons/chevron-down.svg?component';
+import NodeItem from './NodeItem.vue';
 
 export interface NodeCollectionProps {
-  title: string
+    title: string;
 }
 
-interface Tree {
-  id: string
-  title: string
-  children: Node[]
-}
+const props = defineProps<NodeCollectionProps>();
 
-const props = defineProps<NodeCollectionProps>()
+const nodeStore = useNodes();
 
-const tree = computed<Tree>(() => ({
-  id: '0',
-  title: 'Root',
-  children: buildTree(rootIds)
-}))
+nodeStore.fetchNodes();
 </script>
 
 <template>
-  <div :class="styles.collection">
-    <div :class="styles.content">
-      <div class="border border-slate-50 p-6">{{ props.title }}</div>
-      <div :class="styles.inner">
-        <NodeItem root :node="tree" />
-      </div>
+    <div :class="styles.collection">
+        <div :class="styles.content">
+            <div class="border border-slate-50 p-6">{{ props.title }}</div>
+            <div :class="styles.inner">
+                <div :class="styles.lineColumn">
+                    <div v-for="(node, i) in nodeStore.treeRows" :key="i" :class="styles.lineColRow">
+                        <button class="flex justify-center w-full" v-if="node.children.length > 0"
+                            @click="nodeStore.toggleNode(node.id)">
+                            <ChevronDown v-if="node.isExpanded" :class="styles.icon" />
+                            <ChevronRight v-else :class="styles.icon" />
+                        </button>
+                        <span :class="styles.lineColumnEmpty" v-else>
+                        </span>
+                    </div>
+                </div>
+                <div class="flex-1 py-3">
+                    <NodeItem root :node="nodeStore.tree" :tree-line="false" />
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
 const styles = computed(() => ({
-  collection: ['container', 'h-full', 'min-h-[80vh]', 'md:py-2', 'border'],
-  content: ['w-96', 'mx-auto'],
-  inner: ['px-2', 'py-3', 'border']
-}))
+    collection: ['container', 'h-full', 'min-h-[80vh]', 'md:py-2', 'border'],
+    content: ['w-96', 'mx-auto'],
+    inner: ['relative', 'flex', 'border'],
+    lineColumn: ['py-3'],
+    lineColRow: ['px-2', 'leading-6', 'text-center', 'h-6'],
+    icon: ['w-6', 'h-6', 'fill-gray-500'],
+    lineColumnEmpty: ['h-6', 'w-full', 'bg-red-700'],
+}));
 </script>
--- src/components/NodeCollection/NodeItem.vue
+++ src/components/NodeCollection/NodeItem.vue
@@ -1,91 +1,123 @@
 <script setup lang="ts">
-import type { Node } from './nodes'
-import ChevronDown from '../../assets/chevron-down-bold.svg?component'
-import ChevronRight from '../../assets/chevron-right-bold.svg?component'
+import { computed } from 'vue';
+import { useNodes } from '@/stores/nodes';
+import type { Node } from '@/stores/nodes/interfaces';
+import Add from '/public/assets/icons/add-line.svg?component';
+import Close from '/public/assets/icons/close-line.svg?component';
 
 export interface NodeItemProps {
-    node: Node
-    root?: boolean
-    danglingLine?: boolean
+    node: Node;
+    root?: boolean;
+    parentId?: string;
+    danglingLine?: boolean;
+    treeLine?: boolean;
 }
 
-/**
- * !!!!!!!!!!!!!!!!!!!!!! = GIT = !!!!!!!!!!!!!!!!!!!!!!
- */
+const props = defineProps<NodeItemProps>();
+const nodeStore = useNodes();
 
-const props = defineProps<NodeItemProps>()
-
-const isExpanded = ref(true)
-const childrenSize = computed(() => props.node.children.length)
-const hasChildren = computed(() => childrenSize.value > 0)
+const childrenSize = computed(() => props.node.children.length);
+const hasChildren = computed(() => childrenSize.value > 0);
 
 const hasDanglingLine = computed(() => (childNode: Node, idx: number) => {
     // last
-    const isLast = idx > 0 && idx === props.node.children.length - 1
-    const hasManyChildren = childNode.children.length > 0
+    const isLast = idx > 0 && idx === props.node.children.length - 1;
+    const hasManyChildren = childNode.children.length > 0;
 
     // first
     const isFirstWithChildren =
-        idx === 0 && childNode.children.length === 1 && childrenSize.value === 1
+        idx === 0 &&
+        childNode.children.length === 1 &&
+        childrenSize.value === 1;
 
-    return (isLast && hasManyChildren) || isFirstWithChildren
-})
+    return (isLast && hasManyChildren) || isFirstWithChildren;
+});
 
-const toggle = () => (isExpanded.value = !isExpanded.value)
+const add = () => {
+    nodeStore.addIntoNode({
+        parentId: props.node.id,
+        title: `Node ${Math.round(Math.random() * 10000)}`,
+    });
+};
+
+const deleteNode = () => {
+    nodeStore.deleteNode(props.node.id, props.parentId);
+};
 </script>
 
 <template>
-    <div :class="styles.node" data-root="true">
-        <div v-if="danglingLine" :class="styles.groupLineCover" data-cover="true" />
-        <div v-show="!root" :class="styles.content" data-content="true">
-            <button :class="styles.toggleBtn(hasChildren)" @click="toggle">
-                <ChevronDown v-if="isExpanded" :class="styles.icon" />
-                <ChevronRight v-else :class="styles.icon" />
-            </button>
-            <input type="checkbox" :class="styles.checkBtn" />
-            <div :class="styles.title">{{ node.title }}</div>
+    <div :class="styles.node" :data-root="root">
+        <div v-if="danglingLine && treeLine" :class="styles.groupLineCover" data-cover="true" />
+        <div v-show="!root" :class="styles.row" data-content="true">
+            <div :class="styles.content">
+                <div :class="styles.rowContent">
+                    <input type="checkbox" :class="styles.checkbox" />
+                    <div :class="styles.title">{{ node.title }}</div>
+                </div>
+                <div :class="styles.rowControls">
+                    <button @click="add">
+                        <Add :class="styles.ctrlIcon" />
+                    </button>
+                    <button @click="deleteNode">
+                        <Close :class="styles.ctrlIcon" />
+                    </button>
+                </div>
+            </div>
         </div>
 
-        <div v-show="isExpanded" :class="styles.children(!root)" data-children="true">
-            <div :class="styles.groupLine(hasChildren)" />
+        <div v-show="node.isExpanded" :class="styles.children(!root)" data-children="true" :data-id="node.id">
+            <div v-if="treeLine" :class="styles.groupLine(hasChildren)" />
             <NodeItem v-for="(childNode, idx) in node.children" :key="childNode.id" :node="childNode"
-                :dangling-line="hasDanglingLine(childNode, idx)" />
+                :dangling-line="hasDanglingLine(childNode, idx)" :parent-id="node.id" :tree-line="treeLine" />
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { computed, ref } from 'vue'
-
 const styles = computed(() => ({
-    node: ['relative'],
-    content: ['flex', 'px-2', 'py-1'],
+    node: [],
+    row: ['flex', 'px-2', 'hover:bg-neutral-800'],
+    rowContent: ['flex'],
+    rowControls: [
+        'flex',
+        'justify-end',
+        'flex-1',
+        'opacity-0',
+        'hover:opacity-100',
+    ],
+
+    content: ['flex', 'flex-1', 'items-center', 'justify-between'],
     title: ['text-white', 'leading-4', 'text-sm'],
-    children: (leftGutter: boolean) => [leftGutter && 'ml-6'],
+    children: (leftGutter: boolean) => [leftGutter && 'ml-7'],
     groupLine: (withChildren: boolean) => [
         'absolute',
         'top-1',
         'bottom-3',
-        'left-[15px]',
+        'left-[19px]',
         'w-2',
         'border-l',
         'border-l-gray-500',
-        ...[withChildren && ['border-b', 'border-b-gray-500']]
+        ...[withChildren && ['border-b', 'border-b-gray-500']],
     ],
     groupLineCover: [
         'absolute',
         'top-4',
         'bottom-2',
-        'left-2',
+        'left-3',
         'w-4',
         'border-t',
         'border-t-gray-500',
-        'bg-neutral-900'
+        'bg-neutral-900',
     ],
-    icon: ['w-4', 'h-4', 'text-gray-500'],
-    toggleBtn: (show: boolean) => ['bg-neutral-900', 'mr-2', show ? 'visible' : 'invisible'],
-    checkBtn: [
-        'mr-2',
+    icon: ['w-6', 'h-6', 'fill-gray-500'],
+    toggleBtn: (show: boolean) => [
+        'bg-neutral-900',
+        'mr-1',
+        show ? 'visible' : 'invisible',
+    ],
+    ctrlIcon: ['w-6', 'h-6', 'fill-gray-500'],
+    checkbox: [
+        'mr-3',
         'w-4',
         'h-4',
         'rounded-full',
@@ -93,7 +125,7 @@ const styles = computed(() => ({
         'border-gray-600',
         'leading-[.5rem]',
         'text-[10px]',
-        'text-gray-400'
-    ]
-}))
+        'text-gray-400',
+    ],
+}));
 </script>

6ede2eb61b06f550b5a76eac7ffd69975c8f3f7e
ng <ng@192.168.1.16>
2023-08-19 23:33:55 +0800

    styles: update node collection styles

--- src/components/NodeCollection/NodeCollection.vue
+++ src/components/NodeCollection/NodeCollection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useNodes } from '@/stores/nodes';
 import ChevronRight from '/public/assets/icons/chevron-right.svg?component';
 import ChevronDown from '/public/assets/icons/chevron-down.svg?component';
@@ -13,26 +13,28 @@ const props = defineProps<NodeCollectionProps>();
 
 const nodeStore = useNodes();
 
-nodeStore.fetchNodes();
+onMounted(() => {
+    nodeStore.fetchNodes();
+});
 </script>
 
 <template>
-    <div :class="styles.collection">
+    <div :class="styles.module">
         <div :class="styles.content">
-            <div class="border border-slate-50 p-6">{{ props.title }}</div>
+            <div :class="styles.header">{{ props.title }}</div>
             <div :class="styles.inner">
                 <div :class="styles.lineColumn">
                     <div v-for="(node, i) in nodeStore.treeRows" :key="i" :class="styles.lineColRow">
-                        <button class="flex justify-center w-full" v-if="node.children.length > 0"
+                        <button :class="styles.btnToggle" v-if="node.children.length > 0"
                             @click="nodeStore.toggleNode(node.id)">
-                            <ChevronDown v-if="node.isExpanded" :class="styles.icon" />
+                            <ChevronDown v-if="nodeStore.getState(node.id, 'expanded')" :class="styles.icon" />
                             <ChevronRight v-else :class="styles.icon" />
                         </button>
                         <span :class="styles.lineColumnEmpty" v-else>
                         </span>
                     </div>
                 </div>
-                <div class="flex-1 py-3">
+                <div :class="styles.nodeTree">
                     <NodeItem root :node="nodeStore.tree" :tree-line="false" />
                 </div>
             </div>
@@ -42,12 +44,18 @@ nodeStore.fetchNodes();
 
 <script lang="ts">
 const styles = computed(() => ({
-    collection: ['container', 'h-full', 'min-h-[80vh]', 'md:py-2', 'border'],
+    module: ['container', 'h-full', 'min-h-[80vh]', 'md:py-2', 'border'],
     content: ['w-96', 'mx-auto'],
-    inner: ['relative', 'flex', 'border'],
-    lineColumn: ['py-3'],
+    header: ['border', 'border-slate-50', 'p-6'],
+    inner: ['relative', 'flex', 'border', 'py-3'],
+
+    lineColumn: [],
+    nodeTree: ['flex-1'],
+
     lineColRow: ['px-2', 'leading-6', 'text-center', 'h-6'],
-    icon: ['w-6', 'h-6', 'fill-gray-500'],
     lineColumnEmpty: ['h-6', 'w-full', 'bg-red-700'],
+
+    btnToggle: ['flex', 'justify-center', 'w-full'],
+    icon: ['w-6', 'h-6', 'fill-gray-500'],
 }));
 </script>

0e5a9f2daf0406cf680199478aee4d5ba899497d
genriquez <genriquez@proton.me>
2023-09-04 00:20:51 +0800

    feat: add global sass rem util

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    watch: {
-      usePolling: true
-    }
-  }
-})
+    plugins: [vue(), svgLoader()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @use "src/styles/_helpers.scss" as *;
+                `,
+            },
+        },
+    },
+});

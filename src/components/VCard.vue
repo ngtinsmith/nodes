b728edd263e85550a28067b0ed7b2dec8430a272
genriquez <genriquez@proton.me>
2023-09-15 15:52:32 +0800

    feat: remove unused tree line-column

--- src/components/VCard.vue
+++ src/components/VCard.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import { useNodes } from '@/stores/nodes';
-import ChevronRight from '/public/assets/icons/chevron-right.svg?component';
-import ChevronDown from '/public/assets/icons/chevron-down.svg?component';
 
 import ArrowLeft from '/public/assets/icons/arrow-left.svg?component';
 import ArrowRight from '/public/assets/icons/arrow-right.svg?component';
@@ -10,7 +8,6 @@ import Edit from '/public/assets/icons/edit.svg?component';
 import Gear from '/public/assets/icons/gear.svg?component';
 import Save from '/public/assets/icons/save.svg?component';
 import VNode from './VNode.vue';
-import type { NodeId } from '@/stores/nodes/interfaces';
 
 export interface NodeCollectionProps {
     title: string;
@@ -22,16 +19,6 @@ const nodeStore = useNodes();
 
 onMounted(() => {
     nodeStore.fetchNodes();
-});
-
-const isAncestorVisible = computed(() => (nodeId: NodeId) => {
-    const ancestorIds = nodeStore.nodePath[nodeId];
-
-    // rootNodes has no ancestor (null)
-    // rootNodes are always visible
-    if (ancestorIds === null) return true;
-
-    return ancestorIds?.every((id) => nodeStore.nodeStateMap[id].expanded);
 });
 </script>
 
@@ -52,32 +39,6 @@ const isAncestorVisible = computed(() => (nodeId: NodeId) => {
             </div>
         </div>
         <div class="content">
-            <div class="line-column">
-                <div
-                    v-for="node in nodeStore.treeRows"
-                    :key="node.id"
-                    :class="['sign', !isAncestorVisible(node.id) && 'hidden']"
-                >
-                    <button
-                        v-if="node.children.length > 0"
-                        class="sign-toggle"
-                        @click="nodeStore.toggleNode(node.id)"
-                    >
-                        <ChevronDown
-                            v-if="nodeStore.getState(node.id, 'expanded')"
-                            class="sign-icon"
-                        />
-                        <ChevronRight
-                            v-else
-                            class="sign-icon"
-                        />
-                    </button>
-                    <div
-                        v-else
-                        class="sign-empty"
-                    />
-                </div>
-            </div>
             <div class="tree">
                 <VNode
                     root
@@ -137,29 +98,6 @@ const isAncestorVisible = computed(() => (nodeId: NodeId) => {
         background-color: var(--v-slate-900);
         border-radius: rem(4);
         overflow: hidden;
-    }
-
-    .sign {
-        padding-inline: rem(4);
-        height: rem(24);
-        line-height: rem(24);
-        text-align: center;
-        display: none;
-
-        &.hidden {
-            display: none;
-        }
-    }
-
-    .sign-empty {
-        height: rem(24);
-        width: 100%;
-    }
-
-    .sign-toggle {
-        display: flex;
-        justify-content: center;
-        width: 100%;
     }
 
     .sign-icon {

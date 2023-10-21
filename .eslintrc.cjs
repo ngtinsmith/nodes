45dc7cf53282c133e3513dae15f10e1733473812
genriquez <genriquez@proton.me>
2023-10-21 10:44:47 +0800

    chore: format eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
     overrides: [
         {
             files: ['*.ts', '*.tsx', '*.js'],
@@ -23,5 +23,5 @@ module.exports = {
             files: ['*.vue'],
             parser: 'vue-eslint-parser',
         },
-    ]
-}
+    ],
+};
